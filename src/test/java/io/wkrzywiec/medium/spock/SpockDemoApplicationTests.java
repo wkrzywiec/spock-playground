81dca80fbe4230751b3f524b4b7ef1e897c78016
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-10-17 21:26:32 +0200

    init commit

---
+++ src/test/java/io/wkrzywiec/medium/spock/SpockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.medium.spock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
